dd28f7e34d4e0534df47d0618f41ebc3fe7ee318
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-03-11 12:14:29 +0100

    post is implemented

--- src/services/media/index.js
+++ src/services/media/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import uniqid from "uniqid";
 import createHttpError from "http-errors";
 import { getMedia, writeMedia } from "../../lib/fs-tools.js";
+import { validator } from "./validation.js";
+import { validationResult } from "express-validator";
 
 const mediaRouter = express.Router();
 
@@ -21,6 +23,23 @@ mediaRouter.get("/:imdbID", async (req, res, next) => {
       (media) => media.imdbID === req.params.imdbID
     );
     res.send(foundMedia);
+  } catch (error) {
+    next(error);
+  }
+});
+
+mediaRouter.post("/", validator, async (req, res, next) => {
+  try {
+    const errorsList = validationResult(req);
+    if (errorsList.isEmpty()) {
+      const allMedia = await getMedia();
+      const newMedia = { ...req.body, createdAt: new Date(), imdbID: uniqid() };
+      allMedia.push(newMedia);
+      await writeMedia(allMedia);
+      res.status(201).send(newMedia);
+    } else {
+      next(createHttpError(400, "OOPS something went wrong", { errorsList }));
+    }
   } catch (error) {
     next(error);
   }
